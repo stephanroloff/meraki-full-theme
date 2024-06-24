28435f121361369597afcf38649aff0023c88f60
Stephan Roloff <sr@werbeagenten.de>
2024-06-24 12:50:15 +0200

    little changes

--- inc/extra-styles-native-blocks.php
+++ inc/extra-styles-native-blocks.php
@@ -7,25 +7,25 @@ function default_theme_register_block_styles() {
 
     //----BUTTON------------------------------------------------------------------
 
-    // register_block_style(
-    //     'core/button',
-    //     array(
-    //         'name'         => 'button-style-2',
-    //         'label'        => __( 'Button Style 2', 'textdomain' ),
-    //         'is_default'   => false,
-    //     )
-    // );
+    register_block_style(
+        'core/button',
+        array(
+            'name'         => 'button-style-2',
+            'label'        => __( 'Button Style 2', 'textdomain' ),
+            'is_default'   => false,
+        )
+    );
 
     //----GROUP-------------------------------------------------------------------
 
-    // register_block_style(
-    //     'core/group',
-    //     array(
-    //         'name'         => 'group-responsive-padding',
-    //         'label'        => __( 'Add Responsive Padding', 'textdomain' ),
-    //         'is_default'   => false,
-    //     )
-    // );
+    register_block_style(
+        'core/group',
+        array(
+            'name'         => 'group-style-2',
+            'label'        => __( 'Group Position Relative', 'textdomain' ),
+            'is_default'   => false,
+        )
+    );
  
 }
 add_action( 'init', 'default_theme_register_block_styles' );
