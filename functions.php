b3806723c5a9042eec21c9ab975ea223b3f33245
Stephan Roloff <sr@werbeagenten.de>
2024-11-25 11:38:47 +0100

    extra styles native blocks auskommentiert

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 include get_theme_file_path( 'inc/acf-json-support.php' );
 include get_theme_file_path( 'inc/enqueue_assets.php' );
-include get_theme_file_path( 'inc/extra-styles-native-blocks.php' );
+// include get_theme_file_path( 'inc/extra-styles-native-blocks.php' );
 include get_theme_file_path( 'inc/options-page.php' );
 
 // require_once(__DIR__ . '/inc/custom-post-type.php'); //other option to add a file
