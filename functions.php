8ffa02f646248c699fa3f8946c9e28f271bc7d24
Stephan Roloff <sr@werbeagenten.de>
2025-05-23 15:49:01 +0200

    deleting positioning feaure

--- functions.php
+++ functions.php
@@ -2,9 +2,7 @@
 include get_theme_file_path( 'inc/acf-json-support.php' );
 include get_theme_file_path( 'inc/enqueue_assets.php' );
 include get_theme_file_path( 'inc/options-page.php' );
-
 include get_theme_file_path( 'inc/custom-post-type.php' );
 include get_theme_file_path( 'inc/post-type-taxonomy.php' );
-include get_theme_file_path( 'inc/positioning/add-html-attributes.php' );
 include get_theme_file_path( 'inc/unlock-blocks-permissions.php' );
 include get_theme_file_path( 'inc/register-pattern-automatically.php' );
--- src/editor.js
+++ src/editor.js
@@ -1,3 +1,1 @@
 import "./editor.scss";
-import "./js/positioning/createAttribute";
-import "./js/positioning/createOptionsInEditor";
