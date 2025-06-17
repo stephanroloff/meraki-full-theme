e6ccfcaf456382ca75436dd149213732e50e2fae
Stephan Roloff <sr@werbeagenten.de>
2025-06-17 09:33:05 +0200

    new option page for main menu

--- inc/options-page.php
+++ inc/options-page.php
@@ -1,13 +1,14 @@
 <?php
+//In order for this code to work, ACF PRO must be installed.
 
-// if( function_exists('acf_add_options_page') ) {
+if( function_exists('acf_add_options_page') ) {
     
-//     acf_add_options_page(array(
-//         'page_title'    => 'Masonry Galleries',
-//         'menu_title'    => 'Masonry Galleries',
-//         'menu_slug'     => 'masonry-galleries',
-//         'capability'    => 'edit_posts',
-//         'redirect'      => false
-//     ));
+    acf_add_options_page(array(
+        'page_title'    => 'Main Menu',
+        'menu_title'    => 'Main Menu',
+        'menu_slug'     => 'main-menu',
+        'capability'    => 'edit_posts',
+        'redirect'      => false
+    ));
     
-// }
+}
