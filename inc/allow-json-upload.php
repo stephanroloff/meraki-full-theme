15506b4da9ef3cdb9675bbbb82eb62d85219011c
Stephan Roloff <sr@werbeagenten.de>
2026-08-19 15:11:21 +0200

    change allow json upload

--- inc/allow-json-upload.php
+++ inc/allow-json-upload.php
@@ -4,3 +4,12 @@ function allow_json_upload($mimes) {
     return $mimes;
 }
 add_filter('upload_mimes', 'allow_json_upload');
+
+add_filter('wp_check_filetype_and_ext', function($data, $file, $filename, $mimes) {
+    $ext = pathinfo($filename, PATHINFO_EXTENSION);
+    if ($ext === 'json') {
+        $data['ext'] = 'json';
+        $data['type'] = 'application/json';
+    }
+    return $data;
+}, 10, 4);
